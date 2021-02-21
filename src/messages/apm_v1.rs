92aeebc7586c805668729d8af86da42de8410474
Oleksandr Korienev <alexkorienev@gmail.com>
2021-02-21 19:43:29 +0200

    Add application_hash (pod hash) field

--- src/messages/apm_v1.rs
+++ src/messages/apm_v1.rs
@@ -1,11 +1,12 @@
 use deku::prelude::*;
 
-use crate::parts::{Action, Application, Realm, Status, TrackingPart};
+use crate::parts::{Action, Application, ApplicationHash, Realm, Status, TrackingPart};
 
 #[derive(Debug, PartialEq, DekuRead, DekuWrite)]
 pub struct APMV1Message {
     pub realm: Realm,
     pub application: Application,
+    pub application_hash: ApplicationHash,
     pub action: Action,
     pub status: Status,
     pub duration: u64,
--- src/parts/mod.rs
+++ src/parts/mod.rs
@@ -6,6 +6,7 @@ pub type Realm = string::String32;
 pub type Application = string::String32;
 pub type Status = string::String16;
 pub type Action = string::String256;
+pub type ApplicationHash = string::String32;
 
 pub type PartName = string::String32;
 
